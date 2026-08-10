e6098101d520713ab03e3adaac642e07ff94c158
egzamin889-tech <egzamin889@gmail.com>
2026-08-10 20:05:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const gifts = document.querySelectorAll(".gift");
 
 gifts.forEach((gift) => {
 
-    gift.addEventListener("click", () => {
+    gift.addEventListener("click", (event) => {
 
-        if (gift.classList.contains("opened")) {
+        if (
+            gift.classList.contains("opening") ||
+            gift.classList.contains("opened")
+        ) {
             return;
         }
 
@@ -13,9 +16,27 @@ gifts.forEach((gift) => {
         setTimeout(() => {
 
             gift.classList.remove("opening");
+
             gift.classList.add("opened");
 
         }, 800);
+
+    });
+
+});
+
+
+const closeButtons = document.querySelectorAll(".close-gift");
+
+closeButtons.forEach((button) => {
+
+    button.addEventListener("click", (event) => {
+
+        event.stopPropagation();
+
+        const gift = button.closest(".gift");
+
+        gift.classList.remove("opened");
 
     });
 
